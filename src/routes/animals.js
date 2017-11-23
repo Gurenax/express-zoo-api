68783ccb3d277b0d9edeb86b601977ee0f3a309f
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-23 11:35:15 +1100

    Add Animal model

--- src/routes/animals.js
+++ src/routes/animals.js
@@ -1,23 +1,25 @@
 const express = require('express')
+const Animal = require('../models/animals')
 
 const router = express.Router()
 
-let animals = [
-  {
-    name: 'Meercat',
-    count: 27
-  },
-  {
-    name: 'Rhinoceros',
-    count: 4
-  },
-  {
-    name: 'Giraffe',
-    count: 8
-  }
-]
+// let animals = [
+//   {
+//     name: 'Meercat',
+//     count: 27
+//   },
+//   {
+//     name: 'Rhinoceros',
+//     count: 4
+//   },
+//   {
+//     name: 'Giraffe',
+//     count: 8
+//   }
+// ]
 
 router.get('/animals', (req, res) => {
+  const animals = Animal.all()
   res.json(animals)
 })
 
