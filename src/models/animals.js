5e3b84dc105319df095fa2a1cdef2f00fd4a61ae
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-23 11:48:51 +1100

    Update animal model to find specific animal

--- src/models/animals.js
+++ src/models/animals.js
@@ -1,22 +1,38 @@
 let animals = [
   {
+    id: 1,
     name: 'Meercat',
     count: 27
   },
   {
+    id: 2,
     name: 'Rhinoceros',
     count: 4
   },
   {
+    id: 3,
     name: 'Giraffe',
     count: 8
   }
 ]
 
+// Returns all animals
 function all() {
   return animals
 }
 
+// Returns a specific animal
+function find(id) {
+  // Ensure id is an integer
+  id = parseInt(id, 10)
+  // let foundAnimal = null
+  // animals.map( animal => {s
+  //   i
+  // })
+  // Return the animal we found, or if not sucessfull, null
+  return animals[id] === undefined ? null : animals[id]
+}
+
 module.exports = {
-  all
+  all, find
 }
--- src/routes/animals.js
+++ src/routes/animals.js
@@ -3,24 +3,15 @@ const Animal = require('../models/animals')
 
 const router = express.Router()
 
-// let animals = [
-//   {
-//     name: 'Meercat',
-//     count: 27
-//   },
-//   {
-//     name: 'Rhinoceros',
-//     count: 4
-//   },
-//   {
-//     name: 'Giraffe',
-//     count: 8
-//   }
-// ]
-
 router.get('/animals', (req, res) => {
   const animals = Animal.all()
   res.json(animals)
+})
+
+router.get('/animals/:id', (req, res) => {
+  const id = req.params.id
+  const animal = Animal.find(id)
+  res.json(animal)
 })
 
 module.exports = router
